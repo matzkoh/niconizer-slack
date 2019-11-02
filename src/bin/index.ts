3e9137afeeaabf6d88d65a8ab57474c6e96220c0
matzkoh <matzkoh@gmail.com>
2019-11-02 10:48:50 +0900

    fix: make --token required

--- src/bin/index.ts
+++ src/bin/index.ts
@@ -5,8 +5,8 @@ import program from 'commander'
 import { main } from '../lib'
 
 program
+  .requiredOption('-t, --token <token>', 'slack token')
   .option('-u, --url <url>', 'websocket server url', 'ws://localhost:25252')
-  .option('-t, --token <token>', 'slack token')
   .option('-c, --channel <name|id>', 'slack channel name/id')
   .option('-T, --thread', 'include thread reply', false)
   .option('-B, --bot', 'include bot user', false)
